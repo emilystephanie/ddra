77dbd22629b082970c3ba190bc42deb4db3e8d79
Estagio <estagio.ti@contabilivre.com.br>
2020-04-22 15:33:37 -0300

    inicializando

---
+++ index.php
@@ -0,0 +1,27 @@
+<?php
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Home - DDA</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+	<center>
+		<h1>Bem-vindos ao site DDR AGRO</h1>	
+
+		<div class="panel">
+			
+			<img src="img/img.jpg" width="80%">	
+			<a href="https://google.com"><h2>Cadastre-se já</h2></a>
+			
+		</div>
+
+			
+	</center>>
+
+
+	
+</body>
+</html>
